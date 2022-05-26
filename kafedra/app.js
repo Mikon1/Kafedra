9ae44b1c3c0d0634e65b47ea5ac80c604f331626
Mikon1 <105500752+Mikon1@users.noreply.github.com>
2022-05-26 17:10:29 +0500

    Add files via upload

---
+++ kafedra/app.js
@@ -0,0 +1,11 @@
+let joqariga = document.querySelector('.joqariga'), 
+    tomenge = document.querySelector('.tomenge'),
+    suwret = document.querySelector('.suwret'),
+    sanawshi = 
+
+tomenge.addEventListener('click',function(){
+suwret.scroll(0,500);
+})
+joqariga.addEventListener('click',function(){
+suwret.scroll(0,-500);
+})
